ab741f6196a60e706d45e17a71228ec8d9cf96c8
cammc <cam.mclagan@gmail.com>
2025-03-20 12:38:35 -0700

    Added toggle sidebar and animated it. Can be changed

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-
 
 {/* Assets */ }
 import sidebar_icon from './assets/sidebar_icon.svg'
@@ -11,29 +9,51 @@ import './sidebar.css'
 
 function Sidebar() {
 
+  function minimize() {
+    const sidebar = document.getElementById("sidebar");
+    const sizeBox = document.getElementById("size-box");
+    if (!sidebar || !sizeBox) {
+      return;
+    }
+    if (sidebar.style.width === "0px") {
+      sidebar.style.width = "15vw";
+      sidebar.style.minWidth = "200px";
+      sizeBox.style.width = "15vw";
+      setTimeout(() => {
+        sizeBox.style.minWidth = "0";
+      }, 500)
+    }
+    else {
+      sizeBox.style.width = sizeBox.offsetWidth + "px"
+      sizeBox.style.minWidth = (sizeBox.offsetWidth - 40) + "px"
+      sidebar.style.width = "0";
+      sidebar.style.minWidth = "0";
+    }
+  }
+
   return (
     <nav>
-      <div className="sidebar">
+      {/* Minimize Icon */}
+      <div id={"minimize-icon"} className="icon-container">
+        <img className="icon" src={sidebar_icon} onClick={minimize}  alt={"Toggle sidebar icon"}/>
+      </div>
+      <div id={"sidebar"} style={{display: "flex",}} className="sidebar">
+        <div id={"size-box"}>
+          {/* Logo */}
+          <img style={{ width: "85%", padding: "15px 0px", }} src={logo}  alt={"Draw-E logo"}/>
 
-        {/* Minimize Icon */}
-        <div className="icon-container">
-          <img className="icon" src={sidebar_icon} />
-        </div>
+          {/* Search Button */}
+          <div className="search-bar-container">
+            <img className="icon2" src={magnifying_glass}  alt={"Search icon"}/>
+            <input className="search-bar" type="text" placeholder="Search"></input>
+          </div>
 
-        {/* Logo */}
-        <img style={{ width: "85%", padding: "15px 0px", }} src={logo} />
-
-        {/* Search Button */}
-        <div className="search-bar-container">
-          <img className="icon2" src={magnifying_glass} />
-          <input className="search-bar" type="text" placeholder="Search"></input>
-        </div>
-
-        {/* New Project Button*/}
-        <label style={{ fontWeight: "700", fontSize: "20px", }} htmlFor="new-project-button" className="new-project-label">New Project</label>
-        <div id="new-project-button" className="new-project-button">
-          <img className="icon2" src={plus_button} />
-          New Project
+          {/* New Project Button*/}
+          <label style={{ fontWeight: "700", fontSize: "20px", }} htmlFor="new-project-button" className="new-project-label">New Project</label>
+          <div id="new-project-button" className="new-project-button">
+            <img className="icon2" src={plus_button}  alt={"New project icon"}/>
+            New Project
+          </div>
         </div>
       </div>
     </nav>
