aa8c11b04110825adb65ed4c4c4c0282e5a1622e
Eli_b <eli_brekke@sfu.ca>
2025-03-27 14:02:21 -0700

    "Added get Image Description functionality"

--- src/GPT/GPT.ts
+++ src/GPT/GPT.ts
@@ -18,3 +18,32 @@ export async  function generateImage() {
 
     return image.data.at(0);
 }
+
+export async function getImageDescription(imageUrl: string) {
+    const response = await openai.chat.completions.create({
+        model: "gpt-40-mini",
+        messages: [{
+            role: "user",
+            content: [
+                { type: "text", text: "Can you describe this image in detail?"},
+                {
+                    type: "image_url",
+                    image_url: {url: imageUrl,}
+                },
+            ],
+
+        }],
+    });  
+    return response.choices[0].message.content;
+}
+
+//Likely impossible, need backend
+/*export async function editImage(imageUrl: string, prompt: string) {
+    const image = await openai.images.edit({
+        model: "dall-e-2",
+        prompt: prompt,
+        image: imageUrl,
+        n: 1,
+        size: "256x256",
+    })
+*/
