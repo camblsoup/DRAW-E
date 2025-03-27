0451f93b76f32191da050914b777b863b5c048d5
eMongoose <eha80@sfu.ca>
2025-03-27 15:14:27 -0700

    changed "App" to "LandingPage"

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import LandingPage from './LandingPage.tsx'
 import Sidebar from './Sidebar.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Sidebar />
-    <App />
+    <LandingPage />
   </StrictMode>
 )
